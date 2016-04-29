181f2875a0ca7936c4f59bd72ba6ab2d7f4afc43
Cosmin Radoi <cosmin.radoi@gmail.com>
2016-04-29 18:08:15 -0500

    assoc matching

--- src/main/scala/kale/kale.scala
+++ src/main/scala/kale/kale.scala
@@ -1,7 +1,8 @@
 package kale
 
-import scala.collection.{IterableLike, mutable}
 import scala.language.implicitConversions
+import collection._
+import scala.Iterable
 
 object UniqueId {
   var nextId = 0
@@ -153,55 +154,153 @@ case class Variable(name: String) extends Leaf[String] {
   val value = name
 }
 
-object Truth extends LeafLabel[Boolean] with NameFromObject with UniqueId
+object Truth extends LeafLabel[Boolean] with NameFromObject with UniqueId {
+  def apply(v: Boolean) = if (v) Top else Bottom
+}
 
-case class Truth(value: Boolean) extends Leaf[Boolean] with MatcherSolution {
+class Truth(val value: Boolean) extends Leaf[Boolean] with Term {
   val label = Truth
 }
 
-object Top extends Truth(true)
+object Top extends Truth(true) with PureSubstitution {
+  override def get(v: Variable): Option[Term] = None
 
-object Bottom extends Truth(false)
-
-object Binding extends Label2 with NameFromObject with UniqueId
-
-trait MatcherSolution extends Term
-
-case class Binding(_1: Term, _2: Term) extends Node2 with MatcherSolution {
-  assert(_1.isInstanceOf[Variable])
-  val label = Binding
+  override def toString = "⊤"
 }
 
-object Substitution extends Label2 with NameFromObject with UniqueId {
+object Bottom extends Truth(false) {
+  override def toString = "⊥"
+}
+
+object Equality extends Label2 with NameFromObject with UniqueId {
+  override def apply(_1: Term, _2: Term): Equality = _1 match {
+    case v: Variable => new Binding(v, _2)
+    case _ => new Equality(_1, _2)
+  }
+
+  def unapply(t: Term): Option[(Term, Term)] = t match {
+    case e: Equality => Some((e._1, e._2))
+    case _ => None
+  }
+}
+
+trait NonBottom extends Term
+
+object PureSubstitution {
+  def unapply(t: PureSubstitution): Set[Term] = t match {
+    case Top => Set()
+    case b: Binding => Set(b)
+    case s: Substitution => s.iterator.toSet
+  }
+}
+
+trait PureSubstitution extends NonBottom {
+  def get(v: Variable): Option[Term]
+}
+
+class Equality(val _1: Term, val _2: Term) extends Node2 with NonBottom {
+  val label = Equality
+
+  override def equals(other: Any) = other match {
+    case that: Equality => this._1 == that._1 && this._2 == that._2
+    case _ => false
+  }
+}
+
+class Binding(_1: Term, _2: Term) extends Equality(_1, _2) with PureSubstitution {
+  assert(_1.isInstanceOf[Variable])
+
+  def get(v: Variable) = if (_1 == v) Some(_2) else None
+}
+
+trait ConjunctionLabel extends AssocLabel
+
+trait Conjunction extends Node2 with NonBottom
+
+object And extends ConjunctionLabel with NameFromObject with UniqueId {
+
   override def apply(_1: Term, _2: Term): Term = {
-    val m1 = unwrap(_1)
-    val m2 = unwrap(_2)
-    if ((m1.keys.toSet & m2.keys.toSet).forall(v => m1(v) == m2(v))) {
-      apply(m1 ++ m2)
-    } else {
+    if (_1 == Bottom || _2 == Bottom)
       Bottom
+    else {
+      val l1: (PureSubstitution, Iterable[Term]) = unwrap(_1)
+      val l2: (PureSubstitution, Iterable[Term]) = unwrap(_2)
+      Substitution(l1._1, l2._1) match {
+        case Bottom => Bottom
+        case s: PureSubstitution => apply(s, l1._2 ++ l2._2)
+      }
     }
   }
 
-  def unwrap(t: Term) = t match {
+  private def unwrap(t: Term): (PureSubstitution, Iterable[Term]) = t match {
+    case s: PureSubstitution => (s, Iterable.empty)
+    case and: And => (and.s, and.terms)
+    case o => (Top, Iterable(o))
+  }
+
+  def apply(terms: Iterable[Term]): Term = {
+    val bindings: Map[Variable, Term] = terms.collect({ case Equality(v: Variable, t) => v -> t }).toMap
+    val pureSubstitution = Substitution(bindings)
+    val others: Iterable[Term] = terms.filter({ case Equality(v: Variable, t) => false; case _ => true })
+    apply(pureSubstitution, others)
+  }
+
+  def apply(pureSubstitution: PureSubstitution, others: Iterable[Term]): Term = {
+    if (others.isEmpty) {
+      pureSubstitution
+    } else if (pureSubstitution.isEmpty && others.size == 1) {
+      others.head
+    } else {
+      new And(pureSubstitution, others.toSet)
+    }
+  }
+}
+
+final class And(val s: PureSubstitution, val terms: Set[Term]) extends Assoc with NonBottom {
+  assert(!terms.contains(Bottom))
+  val label = And
+
+  lazy val _1: Term = terms.head
+  lazy val _2: Term = And(s, terms.tail)
+  override val list: Iterable[Term] = PureSubstitution.unapply(s) ++ terms
+}
+
+object Substitution extends ConjunctionLabel with NameFromObject with UniqueId {
+  override def apply(_1: Term, _2: Term): Term = {
+    if (_1 == Bottom || _2 == Bottom)
+      Bottom
+    else {
+      val m1 = unwrap(_1)
+      val m2 = unwrap(_2)
+      if ((m1.keys.toSet & m2.keys.toSet).forall(v => m1(v) == m2(v))) {
+        apply(m1 ++ m2)
+      } else {
+        Bottom
+      }
+    }
+  }
+
+  private def unwrap(t: Term) = t match {
     case Top => Map[Variable, Term]()
-    case Binding(_1, _2) => Map[Variable, Term](_1.asInstanceOf[Variable] -> _2)
+    case Equality(_1: Variable, _2) => Map[Variable, Term](_1.asInstanceOf[Variable] -> _2)
     case Substitution(m) => m
   }
 
-  def apply(m: Map[Variable, Term]): MatcherSolution = m.size match {
+  def apply(l: Iterable[Term]) = l.foldLeft(Top: Term)(apply)
+
+  def apply(m: Map[Variable, Term]): PureSubstitution = m.size match {
     case 0 => Top
-    case 1 => Binding(m.head._1, m.head._2)
+    case 1 => new Binding(m.head._1, m.head._2)
     case _ => new Substitution(m)
   }
 
   def unapply(arg: Substitution): Option[Map[Variable, Term]] = Some(arg.m)
 }
 
-final class Substitution(val m: Map[Variable, Term]) extends Node2 with MatcherSolution {
+final class Substitution(val m: Map[Variable, Term]) extends Conjunction with PureSubstitution {
   assert(m.size >= 2)
   val label = Substitution
-  lazy val _1 = Binding(m.head._1, m.head._2)
+  lazy val _1 = Equality(m.head._1, m.head._2)
   lazy val _2 = Substitution(m.tail)
 
   override def equals(other: Any): Boolean = other match {
@@ -210,6 +309,94 @@ final class Substitution(val m: Map[Variable, Term]) extends Node2 with MatcherS
   }
 
   override def hashCode(): Int = label.hashCode
+
+  def get(v: Variable) = m.get(v)
+}
+
+object Or extends AssocLabel with NameFromObject with UniqueId {
+  def apply(_1: Term, _2: Term): Term =
+    unwrap(_1) | unwrap(_2) match {
+      case s if s.isEmpty => Bottom
+      case s if s.size == 1 => s.head
+      case s => Or(s)
+    }
+
+  def unwrap(t: Term): Set[Term] = t match {
+    case o: Or => o.terms
+    case Bottom => Set()
+    case o => Set(o)
+  }
+
+  def apply(l: Iterable[Term]): Term = l.foldLeft(Bottom: Term)(apply)
+}
+
+case class Or(terms: Set[Term]) extends Assoc with NonBottom {
+  assert(terms.size > 1)
+  val label = Or
+
+  lazy val _1 = terms.head
+  lazy val _2 = Or(terms.tail.toSeq)
+  override val list: Iterable[Term] = terms
+}
+
+trait AssocLabel extends Label2 {
+  def apply(l: Iterable[Term]): Term
+
+  val thisthis = this
+
+  def asList(t: Term) = t.label match {
+    case `thisthis` => t.asInstanceOf[Assoc].list
+    case _ => List(t)
+  }
+
+  val listUnapplier = new {
+    def unapplySeq(t: Term): Iterable[Term] = asList(t)
+  }
+}
+
+trait HasId {
+  val identity: Term
+}
+
+trait AssocWithIdLabel extends AssocLabel with HasId {
+  def apply(_1: Term, _2: Term) = {
+    val l1 = unwrap(_1)
+    val l2 = unwrap(_2)
+    apply(l1 ++ l2)
+  }
+
+  def unwrap(t: Term) = t match {
+    case `identity` => List[Term]()
+    case x if x.label == this => x.asInstanceOf[Assoc].list
+    case y => List(y)
+  }
+
+  def apply(l: Iterable[Term]): Term = l match {
+    case l if l.isEmpty => identity
+    case l if l.size == 1 => l.head
+    case _ => construct(l)
+  }
+
+  def construct(l: Iterable[Term]): Term
+}
+
+trait AssocWithoutIdLabel extends AssocLabel {
+
+}
+
+trait Assoc extends Node2 {
+  override val label: AssocLabel
+  val list: Iterable[Term]
+}
+
+case class AssocWithIdListLabel(name: String, identity: Term) extends AssocWithIdLabel with UniqueId {
+  override def construct(l: Iterable[Term]): Term = new AssocWithIdList(this, l)
+}
+
+case class AssocWithIdList(label: AssocLabel, list: Iterable[Term]) extends Assoc {
+  override def _1: Term = list.head
+
+  override def _2: Term = label(list.tail)
 }
 
 trait Hook {
@@ -219,8 +406,6 @@ trait Hook {
 
 trait UnifierFunction[Left <: Term, Right <: Term, Result <: Term] extends (DispatchState => ((Term, Term) => Term)) {
   def apply(solver: DispatchState) = { (a: Term, b: Term) => f(solver)(a.asInstanceOf[Left], b.asInstanceOf[Right]) }
-
-  def foo(solver: DispatchState)(a: Term, b: Term): Term = f(solver)(a.asInstanceOf[Left], b.asInstanceOf[Right])
 
   def f(solver: DispatchState)(a: Left, b: Right): Result
 }
@@ -241,13 +426,72 @@ class Dispatch(pieces: Set[UnifierPiece], maxId: Int) extends DispatchState {
 
   def apply(left: Term, right: Term): Term = {
     val u = arr(left.label.id)(right.label.id)
-    if(u != null)
+    if (u != null)
       u(left, right)
     else
       Bottom
   }
 }
 
+trait UnaryState {
+  def apply(t: Term): Term
+}
+
+trait UnaryFunction[Element <: Term, Result <: Term, US <: UnaryState] extends (US => Term => Term) {
+  def apply(unaryState: US) = { t: Term => f(unaryState)(t.asInstanceOf[Element]) }
+
+  def f(state: US)(t: Element): Result
+}
+
+case class UnaryPiece[US <: UnaryState](label: Label, f: US => Term => Term)
+
+abstract class Application[US <: UnaryState](pieces: Set[UnaryPiece[US]], maxId: Int) extends UnaryState {
+  this: US =>
+  val arr = new Array[Term => Term](maxId)
+
+  for (p <- pieces) {
+    arr(p.label.id) = p.f(this)
+  }
+
+  def apply(t: Term): Term
+}
+
+object SubstitutionApplication {
+  def apply(pieces: Set[UnaryPiece[SubstitutionApplication]], maxId: Int)(s: PureSubstitution) = new SubstitutionApplication(pieces, maxId)(s)
+}
+
+class SubstitutionApplication(pieces: Set[UnaryPiece[SubstitutionApplication]], maxId: Int)(s: PureSubstitution) extends Application[SubstitutionApplication](pieces, maxId) {
+  def get(v: Variable): Option[Term] = s.get(v)
+
+  def apply(t: Term) = arr(t.label.id) match {
+    case null => Bottom
+    case f => f(t)
+  }
+}
+
+object ApplySubstitution {
+
+  object FreeNode0 extends UnaryFunction[Node0, Node0, SubstitutionApplication] {
+    def f(solver: SubstitutionApplication)(t: Node0) = t
+  }
+
+  object FreeNode1 extends UnaryFunction[Node1, Term, SubstitutionApplication] {
+    def f(solver: SubstitutionApplication)(t: Node1) = t.label(solver(t._1))
+  }
+
+  object Node2 extends UnaryFunction[Node2, Term, SubstitutionApplication] {
+    def f(solver: SubstitutionApplication)(t: Node2) = t.label(solver(t._1), solver(t._2))
+  }
+
+  object Var extends UnaryFunction[Variable, Term, SubstitutionApplication] {
+    def f(solver: SubstitutionApplication)(v: Variable) = solver.get(v).getOrElse(v)
+  }
+
+  object Constant extends UnaryFunction[Constant[_], Constant[_], SubstitutionApplication] {
+    def f(solver: SubstitutionApplication)(a: Constant[_]) = a
+  }
+
+}
 
 object SimpleMatcher {
 
@@ -263,11 +507,60 @@ object SimpleMatcher {
     def f(solver: DispatchState)(a: Node2, b: Node2) = Substitution(solver(a._1, b._1), solver(a._2, b._2))
   }
 
-  object VarLeft extends UnifierFunction[Variable, Term, MatcherSolution] {
-    def f(solver: DispatchState)(a: Variable, b: Term) = Binding(a.asInstanceOf[Variable], b)
+  def matchContents(l: AssocLabel, ksL: Iterable[Term], ksR: Iterable[Term])(implicit solver: DispatchState): Term = {
+    val res = (ksL, ksR) match {
+      case (Seq(), Seq()) => Top
+      case ((v: Variable) +: tailL, ksR) =>
+        (0 to ksR.size)
+          .map { index => (ksR.take(index), ksR.drop(index)) }
+          .map { case (prefix, suffix) => And(Equality(v, l(prefix)), matchContents(l, tailL, suffix)) }
+          .fold(Bottom)({ (a, b) => Or(a, b) })
+      case (left, right) if left.nonEmpty && right.nonEmpty => And(solver(left.head, right.head), matchContents(l, left.tail, right.tail): Term)
+      case other => Bottom
+    }
+    res
   }
 
-  object Constants extends UnifierFunction[Constant[_], Constant[_], MatcherSolution] {
+
+  object AssocTerm extends UnifierFunction[Assoc, Term, Term] {
+    def f(solver: DispatchState)(a: Assoc, b: Term) = {
+      val asList = a.label.asList _
+      val l1 = asList(a)
+      val l2 = asList(b)
+      matchContents(a.label, l1, l2)(solver)
+    }
+  }
+
+  object TermAssoc extends UnifierFunction[Term, Assoc, Term] {
+    def f(solver: DispatchState)(a: Term, b: Assoc) = {
+      val asList = b.label.asList _
+      val l1 = asList(a)
+      val l2 = asList(b)
+      matchContents(b.label, l1, l2)(solver)
+    }
+  }
+
+  //  object AssocListIdentity extends UnifierFunction[AssocWithIdList, Term, MatcherSolution] {
+  //    def f(solver: DispatchState)(a: AssocWithIdList, b: Term) = ???
+  //  }
+  //
+  //  object IdentityAssocList extends UnifierFunction[Term, AssocWithIdList, MatcherSolution] {
+  //    def f(solver: DispatchState)(a: Term, b: AssocWithIdList) = ???
+  //  }
+  //
+  //  object AssocListElement extends UnifierFunction[AssocWithIdList, Term, MatcherSolution] {
+  //    def f(solver: DispatchState)(a: AssocWithIdList, b: Term) = ???
+  //  }
+  //
+  //  object ElementAssocList extends UnifierFunction[Term, AssocWithIdList, MatcherSolution] {
+  //    def f(solver: DispatchState)(a: Term, b: AssocWithIdList) = ???
+  //  }
+
+  object VarLeft extends UnifierFunction[Variable, Term, Term] {
+    def f(solver: DispatchState)(a: Variable, b: Term) = Equality(a.asInstanceOf[Variable], b)
+  }
+
+  object Constants extends UnifierFunction[Constant[_], Constant[_], Term] {
     override def f(solver: DispatchState)(a: Constant[_], b: Constant[_]) =
       Truth(a.value == b.value)
   }
--- src/test/scala/kale/MatchSpec.scala
+++ src/test/scala/kale/MatchSpec.scala
@@ -2,6 +2,9 @@ package kale
 
 import org.scalatest.FreeSpec
 
+import collection._
+import scala.language.implicitConversions
+
 class MatchSpec extends FreeSpec {
 
   import Implicits._
@@ -9,31 +12,85 @@ class MatchSpec extends FreeSpec {
   val X = Variable("X")
   val Y = Variable("Y")
 
+  val emptyList = FreeLabel0(UniqueId(), ".List")
+  val listLabel = AssocWithIdListLabel("_,_", emptyList())
+
+  val unifier = new Dispatch(Set(
+    UnifierPiece(Variable, INT, SimpleMatcher.VarLeft),
+    UnifierPiece(INT.+, INT.+, SimpleMatcher.FreeNode2FreeNode2),
+    UnifierPiece(INT, INT, SimpleMatcher.Constants),
+    UnifierPiece(listLabel, listLabel, SimpleMatcher.AssocTerm),
+    UnifierPiece(listLabel, INT, SimpleMatcher.AssocTerm),
+    UnifierPiece(INT, listLabel, SimpleMatcher.TermAssoc),
+    UnifierPiece(listLabel, INT.+, SimpleMatcher.AssocTerm),
+    UnifierPiece(INT.+, listLabel, SimpleMatcher.TermAssoc),
+    UnifierPiece(listLabel, emptyList, SimpleMatcher.AssocTerm),
+    UnifierPiece(emptyList, listLabel, SimpleMatcher.TermAssoc),
+    UnifierPiece(listLabel, Variable, SimpleMatcher.AssocTerm),
+    UnifierPiece(Variable, listLabel, SimpleMatcher.TermAssoc)
+  ), 30)
+
+  val substitutionApplier = SubstitutionApplication(Set(
+    UnaryPiece(Variable, ApplySubstitution.Var),
+    UnaryPiece(INT.+, ApplySubstitution.Node2),
+    UnaryPiece(INT, ApplySubstitution.Constant),
+    UnaryPiece(emptyList, ApplySubstitution.FreeNode0),
+    UnaryPiece(listLabel, ApplySubstitution.Node2)
+  ), 30) _
+
   "simple" - {
-
-    val pieces = Set(
-      UnifierPiece(Variable, INT, SimpleMatcher.VarLeft),
-      UnifierPiece(INT.+, INT.+, SimpleMatcher.FreeNode2FreeNode2)
-    )
-    val unifier = new Dispatch(pieces, 10)
-
-    assert(unifier(X, 5) === Binding(X, 5))
-    assert(unifier(X + Y, (5: Term) + 7) === Substitution(Map(X -> 5, Y -> 7)))
-//    assert((2: Term).unify(5: Term) == Bottom)
-//    assert((2: Term).unify(2: Term) == Top)
+    assert(unifier(X, 5) === Equality(X, 5))
+    assert(unifier(X + Y, (5: Term) + 7) === Substitution(Map(X -> (5: Term), Y -> (7: Term))))
+    assert(unifier(X + X, (5: Term) + 7) === Bottom)
+    assert(unifier((5: Term) + 7, (5: Term) + 7) === Top)
+    //    assert((2: Term).unify(5: Term) == Bottom)
+    //    assert((2: Term).unify(2: Term) == Top)
   }
 
-//  "LIST" in {
-//    val l = new ASSOC_LIST("_,_", 0)
-//    val x = l.op(1, 2)
-//    val y = l.op(3, 4)
-//    assert(x.unify(x) == Top)
-//    assert(x.unify(y) == Bottom)
-//    val z = Substitutions.Pair(X, x)
-//    assert(X.unify(x) == Substitutions.Pair(X, x))
-//    assert(l.op(X, 2).unify(x) == Substitutions.Pair(X, 1))
-//    assert(l.op(X, Y).unify(x) == Or(
-//      And(And(And(X, 1), And(Y, 2)),
-//        And(And(X, 0), And(Y, l.op(1, 2)))),
-//      And(And(X, l.op(1, 2)), And(Y, 0))))
+  "assoc" - {
+    assert(unifier(listLabel(X, 5), listLabel(3, 5)) === Equality(X, 3))
+    assert(unifier(listLabel(List(3, 4, X, 7)), listLabel(List(3, 4, 5, 6, 7))) === Equality(X, listLabel(5, 6)))
+    assert(unifier(listLabel(List(3, X, 5, Y, 7)), listLabel(List(3, 4, 5, 6, 7))) === Substitution(Map(X -> (4: Term), Y -> (6: Term))))
+    assert(unifier(listLabel(List(X, 5, Y)), listLabel(List(3, 4, 5, 6, 7))) === Substitution(Map(X -> listLabel(3, 4), Y -> listLabel(6, 7))))
+    val res = unifier(listLabel(List(3, X, Y, 6)), listLabel(List(3, 4, 5, 6)))
+    println(res)
+    assert(unifier(listLabel(List(3, X, Y, 6)), listLabel(List(3, 4, 5, 6))) ===
+      Or(List(
+        Substitution(Map(X -> emptyList(), Y -> listLabel(4, 5))),
+        Substitution(Map(X -> (4:Term), Y -> (5:Term))),
+        Substitution(Map(X -> listLabel(4, 5), Y -> emptyList()))))
+    )
+  }
+
+  "substitution" - {
+    val s = Substitution(Map(X -> (5: Term)))
+    val substitution = substitutionApplier(s)
+
+    assert(substitution(5) === (5: Term))
+    assert(substitution(X) === (5: Term))
+    assert(substitution(Y) === Y)
+    assert(substitution(Y + X) === Y + 5)
+  }
+
+  "rewrite X + 0 => X" - {
+    assert(substitutionApplier(unifier(X + 0, (5: Term) + 0).asInstanceOf[PureSubstitution])(X) === (5: Term))
+  }
+
+  "rewrite 2 + X + 3 => 5 + X" - {
+    assert(substitutionApplier(unifier((2: Term) + X + 3, (2: Term) + 4 + 3).asInstanceOf[PureSubstitution])((5: Term) + X) === (5: Term) + 4)
+  }
+
+  //  "LIST" in {
+  //    val l = new ASSOC_LIST("_,_", 0)
+  //    val x = l.op(1, 2)
+  //    val y = l.op(3, 4)
+  //    assert(x.unify(x) == Top)
+  //    assert(x.unify(y) == Bottom)
+  //    val z = Substitutions.Pair(X, x)
+  //    assert(X.unify(x) == Substitutions.Pair(X, x))
+  //    assert(l.op(X, 2).unify(x) == Substitutions.Pair(X, 1))
+  //    assert(l.op(X, Y).unify(x) == Or(
+  //      And(And(And(X, 1), And(Y, 2)),
+  //        And(And(X, 0), And(Y, l.op(1, 2)))),
+  //      And(And(X, l.op(1, 2)), And(Y, 0))))
 }
