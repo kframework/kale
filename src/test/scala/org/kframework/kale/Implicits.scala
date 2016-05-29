9ce922db16b472d2426fa07b6d043cd931ace428
Cosmin Radoi <cosmin.radoi@gmail.com>
2016-05-29 09:33:42 -0500

    minor

--- src/test/scala/org/kframework/kale/Implicits.scala
+++ src/test/scala/org/kframework/kale/Implicits.scala
@@ -7,6 +7,6 @@ object Implicits {
   implicit def intConstant(x: Int): Constant[Int] = INT(x)
 
   implicit class asTerm(x: Term) {
-    def +(y: Term) = INT.+(x, y)
+    def +(y: Term): Node2 = INT.+(x, y)
   }
 }
