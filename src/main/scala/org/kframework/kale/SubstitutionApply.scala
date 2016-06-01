26c52282ad21a2c3908da924eae4ad5e4b18d630
Cosmin Radoi <cosmin.radoi@gmail.com>
2016-06-01 10:30:34 -0500

    fixpoint

--- src/main/scala/org/kframework/kale/SubstitutionApply.scala
+++ src/main/scala/org/kframework/kale/SubstitutionApply.scala
@@ -69,6 +69,14 @@ object SubstitutionApply {
 class SubstitutionApply(val pieces: Set[Unary.Piece[SubstitutionApply]], val maxId: Int)(val s: Substitution) extends Unary.Apply[SubstitutionApply](pieces, maxId) with (Term => Term) {
   def getVariable(v: Variable): Option[Term] = s.get(v)
 
+  def fixpoint(t: Term): Term = {
+    val after = apply(t)
+    if (after != t)
+      fixpoint(after)
+    else
+      after
+  }
+
   def apply(t: Term) =
     if (t.isGround)
       t
