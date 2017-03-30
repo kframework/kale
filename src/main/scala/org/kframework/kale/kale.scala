7ec4fc43ce49449223828866eca0049d729ae177
Cosmin Radoi <cosmin.radoi@gmail.com>
2017-03-30 09:13:39 -0500

    refactoring

--- src/main/scala/org/kframework/kale/kale.scala
+++ src/main/scala/org/kframework/kale/kale.scala
@@ -50,60 +50,12 @@ trait LeafLabel[T] extends Label {
   }
 }
 
-trait Key[T] {
-  def name: String = this.getClass.getName
-}
-
-trait Att[T] {
-  def value: T
-
-  def key: Class[_ <: Att[T]] = this.getClass.asInstanceOf[Class[Att[T]]]
-
-  def update(term: Term): Att[T]
-
-  def update(term: Term, oldChildren: Iterable[Term]): Att[T]
-}
-
-case class SimpleAtt[T](value: T) extends Att[T] {
-  override def update(term: Term): Att[T] = this
-  override def update(term: Term, oldChildren: Iterable[Term]): Att[T] = this
-}
-
-trait Term extends Iterable[Term] with pattern.Pattern {
+trait Term extends Iterable[Term] with pattern.Pattern with HasAtt {
   def updateAt(i: Int)(t: Term): Term
 
   val label: Label
 
   val isGround: Boolean
-
-  private var att: Any = null
-
-  def setHiddenAttDONOTUSE(att: Any) = this.att = att
-
-  def getHiddenAttDONOTUSE = this.att
-
-  //  protected[this]
-  var attributes = Map[Class[_], Att[_]]()
-
-  def updatedAttributes(newTerms: Term*): Map[Class[_], Att[_]] = (this.attributes map {
-    case (k, v) => (k, v.update(this, newTerms))
-  }).toMap
-
-  def setAtt(value: Att[_]): Term = {
-    attributes = attributes + (value.getClass -> value)
-    this
-  }
-
-  def getAtt[T <: Att[_]](key: Class[T]): T = attributes(key).asInstanceOf[T]
-
-  def getAttOption[T <: Att[_]](key: Class[T]): Option[T] = attributes.get(key).asInstanceOf[Option[T]]
-
-  def hasAtt[T <: Att[_]](key: Class[T]): Boolean = attributes.contains(key)
-
-  def setAtts(atts: Map[Class[_], Att[_]]): Term = {
-    attributes = atts
-    this
-  }
 
   def iterator(): Iterator[Term]
 
