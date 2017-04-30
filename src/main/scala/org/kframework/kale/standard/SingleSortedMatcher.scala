c4be799b00c4bba6e2c8042b316e69b82553cddf
Cosmin Radoi <cosmin.radoi@gmail.com>
2017-04-30 17:02:56 -0500

    finish refactoring

--- src/main/scala/org/kframework/kale/standard/SingleSortedMatcher.scala
+++ src/main/scala/org/kframework/kale/standard/SingleSortedMatcher.scala
@@ -47,128 +47,116 @@ class SingleSortedMatcher()(implicit val env: StandardEnvironment) extends Match
     res
   }
 
-  object AssocTerm extends ProcessingFunction[Apply] with TypedWith[Assoc, Term] {
-    def f(solver: Apply)(a: Assoc, b: Term) = {
-      val asList = a.label.asList _
-      val l1 = asList(a)
-      val l2 = asList(b)
-      matchContents(a.label, Top, l1, l2)(solver)
-    }
+  def AssocTerm(solver: Apply)(a: Assoc, b: Term) = {
+    val asList = a.label.asList _
+    val l1 = asList(a)
+    val l2 = asList(b)
+    matchContents(a.label, Top, l1, l2)(solver)
   }
 
-  object TermAssoc extends ProcessingFunction[Apply] with TypedWith[Term, Assoc] {
-    def f(solver: Apply)(a: Term, b: Assoc) = {
-      val asList = b.label.asList _
-      val l1 = asList(a)
-      val l2 = asList(b)
-      matchContents(b.label, Top, l1, l2)(solver)
-    }
+  def TermAssoc(solver: Apply)(a: Term, b: Assoc) = {
+    val asList = b.label.asList _
+    val l1 = asList(a)
+    val l2 = asList(b)
+    matchContents(b.label, Top, l1, l2)(solver)
   }
 
-  object MapTerm extends ProcessingFunction[Apply] with TypedWith[Term, Term] {
-    override def f(solver: Apply)(a: Term, b: Term): Term = a.label match {
-      case mapLabel: MapLabel =>
-        val mapLabel.map(left, leftUnindexed) = a
-        val mapLabel.map(right, rightUnindexed) = b
+  def MapTerm(solver: Apply)(a: Term, b: Term): Term = a.label match {
+    case mapLabel: MapLabel =>
+      val mapLabel.map(left, leftUnindexed) = a
+      val mapLabel.map(right, rightUnindexed) = b
 
-        assert(left.size + leftUnindexed.size > 1, "There is some bug in the Piece registration")
+      assert(left.size + leftUnindexed.size > 1, "There is some bug in the Piece registration")
 
-        if (rightUnindexed.nonEmpty) {
-          throw MatchNotSupporteredError(a, b, "Var on the rhs.")
+      if (rightUnindexed.nonEmpty) {
+        throw MatchNotSupporteredError(a, b, "Var on the rhs.")
+      }
+      else if (left.nonEmpty && right.isEmpty && rightUnindexed.isEmpty) {
+        Bottom
+      }
+      else if (left.nonEmpty && right.nonEmpty && leftUnindexed.size <= 1 && rightUnindexed.isEmpty) {
+        val leftKeys = left.keys.toSet
+        val rightKeys = right.keys.toSet
+
+
+        if (!rightKeys.forall(_.isGround)) {
+          throw MatchNotSupporteredError(a, b)
         }
-        else if (left.nonEmpty && right.isEmpty && rightUnindexed.isEmpty) {
+
+        if (!(leftKeys filter (_.isGround) forall rightKeys.contains)) {
           Bottom
         }
-        else if (left.nonEmpty && right.nonEmpty && leftUnindexed.size <= 1 && rightUnindexed.isEmpty) {
-          val leftKeys = left.keys.toSet
-          val rightKeys = right.keys.toSet
+        else if (leftKeys.size - (leftKeys & rightKeys).size <= 1) {
 
+          val commonKeys = leftKeys & rightKeys
 
-          if (!rightKeys.forall(_.isGround)) {
+          val valueMatches = if (commonKeys.nonEmpty)
+            And(commonKeys map (k => solver(left(k), right(k))))
+          else
+            Top
+
+          val lookupByKeyVariableAndValueMatch = if (leftKeys.size - commonKeys.size == 1) {
+            val v = (leftKeys -- rightKeys).head
+            val rightValue = (rightKeys -- leftKeys).head
+
+            And(Equality(v, rightValue), left(v), right(rightValue))
+          } else {
+            Top
+          }
+
+          val freeLeftVariableEquality = if (leftUnindexed.size == 1) {
+            Equality(leftUnindexed.head, mapLabel((rightKeys -- leftKeys).map(right)))
+          } else {
+            Top
+          }
+
+          if (lookupByKeyVariableAndValueMatch != Top && freeLeftVariableEquality != Top) {
             throw MatchNotSupporteredError(a, b)
           }
 
-          if (!(leftKeys filter (_.isGround) forall rightKeys.contains)) {
-            Bottom
-          }
-          else if (leftKeys.size - (leftKeys & rightKeys).size <= 1) {
-
-            val commonKeys = leftKeys & rightKeys
-
-            val valueMatches = if (commonKeys.nonEmpty)
-              And(commonKeys map (k => solver(left(k), right(k))))
-            else
-              Top
-
-            val lookupByKeyVariableAndValueMatch = if (leftKeys.size - commonKeys.size == 1) {
-              val v = (leftKeys -- rightKeys).head
-              val rightValue = (rightKeys -- leftKeys).head
-
-              And(Equality(v, rightValue), left(v), right(rightValue))
-            } else {
-              Top
-            }
-
-            val freeLeftVariableEquality = if (leftUnindexed.size == 1) {
-              Equality(leftUnindexed.head, mapLabel((rightKeys -- leftKeys).map(right)))
-            } else {
-              Top
-            }
-
-            if (lookupByKeyVariableAndValueMatch != Top && freeLeftVariableEquality != Top) {
-              throw MatchNotSupporteredError(a, b)
-            }
-
-            And(valueMatches, lookupByKeyVariableAndValueMatch, freeLeftVariableEquality)
-          } else {
-            throw MatchNotSupporteredError(a, b, "Only supported matches with at most one differing (i.e., symbolic somehow) key and at most a variable (at the top level) on the rhs.")
-          }
+          And(valueMatches, lookupByKeyVariableAndValueMatch, freeLeftVariableEquality)
+        } else {
+          throw MatchNotSupporteredError(a, b, "Only supported matches with at most one differing (i.e., symbolic somehow) key and at most a variable (at the top level) on the rhs.")
         }
-        else {
-          throw MatchNotSupporteredError(a, b, "Not yet implemented. Should eventually default to AC.")
-        }
-    }
+      }
+      else {
+        throw MatchNotSupporteredError(a, b, "Not yet implemented. Should eventually default to AC.")
+      }
   }
 
   // TODO: something is not quite right with FormulaLabel -- make sure it is correct
-  object OneIsFormula extends ProcessingFunction[Apply] with TypedWith[Term, Term] {
-    def f(solver: Apply)(a: Term, b: Term) = And(a, b)
+  def OneIsFormula(solver: Apply)(a: Term, b: Term) = And(a, b)
+
+  def IfThenElseTerm(solver: Apply)(a: Node3, b: Term): Term = {
+    val c = solver(a._1, b)
+    if (c == Bottom)
+      a._3
+    else
+      And(c, a._2)
   }
 
-  object IfThenElseTerm extends ProcessingFunction[Apply] with TypedWith[Node3, Term] {
-    override def f(solver: Apply)(a: Node3, b: Term): Term = {
-      val c = solver(a._1, b)
-      if (c == Bottom)
-        a._3
-      else
-        And(c, a._2)
-    }
-  }
-
-  object BindMatchMatcher extends ProcessingFunction[Apply] with TypedWith[Node2, Term] {
-    override def f(solver: Apply)(a: Node2, b: Term): Term = {
-      val v = a._1.asInstanceOf[Variable]
-      val p = a._2
-      Or(Or.asSet(b) map { bx =>
-        val sol = solver(p, bx)
-        And(Equality(v, bx), sol)
-      })
-    }
+  def BindMatchMatcher(solver: Apply)(a: Node2, b: Term): Term = {
+    val v = a._1.asInstanceOf[Variable]
+    val p = a._2
+    Or(Or.asSet(b) map { bx =>
+      val sol = solver(p, bx)
+      And(Equality(v, bx), sol)
+    })
   }
 
   import standard._
 
   override def processingFunctions: ProcessingFunctions = definePartialFunction({
-    case (`BindMatch`, _) => BindMatchMatcher
-    case (`IfThenElse`, _) => IfThenElseTerm
-    case (_, `Not`) => OneIsFormula
-    case (`Not`, _) => OneIsFormula
+    case (`BindMatch`, _) => BindMatchMatcher _
+    case (`IfThenElse`, _) => IfThenElseTerm _
+    case (_, `Not`) => OneIsFormula _
+    case (`Not`, _) => OneIsFormula _
     case (`And`, _) => AndTerm _
     case (_, `And`) => TermAnd _
     case (`Or`, _) => OrTerm _
     case (_, `Or`) => TermOr _
     case (`Variable`, _) => VarLeft _
- // case (_, `Variable`) => VarRight
+    // case (_, `Variable`) => VarRight
     case (`AnywhereContext`, _) => new AnywhereContextMatcher()(env)
     case (capp: PatternContextApplicationLabel, _) => new PatternContextMatcher()(env)
     case (l1: FreeLabel, l2: FreeLabel) if l1 != l2 => NoMatch _
@@ -183,7 +171,7 @@ class SingleSortedMatcher()(implicit val env: StandardEnvironment) extends Match
     case (_: FunctionDefinedByRewritingLabel3, _: FunctionDefinedByRewritingLabel3) => FreeNode3FreeNode3 _
     case (_: FunctionDefinedByRewritingLabel4, _: FunctionDefinedByRewritingLabel4) => FreeNode4FreeNode4 _
     case (_: DomainValueLabel[_], _: DomainValueLabel[_]) => Constants _
-    case (_: MapLabel, right) if !right.isInstanceOf[Variable] => MapTerm
-    case (_: AssocLabel, right) if !right.isInstanceOf[Variable] => AssocTerm
+    case (_: MapLabel, right) if !right.isInstanceOf[Variable] => MapTerm _
+    case (_: AssocLabel, right) if !right.isInstanceOf[Variable] => AssocTerm _
   }) orElse super.processingFunctions
 }
