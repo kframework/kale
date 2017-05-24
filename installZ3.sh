6fd460846700c8326b7c0163cc936c10612b004c
Manasvi Saxena <msaxena2@illinois.edu>
2017-05-23 19:51:20 -0500

    fix build

--- installZ3.sh
+++ installZ3.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 
+rm -r z3*
 if [ ! -d ./z3 ]; then
 
     URL="https://github.com/Z3Prover/z3/releases/download/z3-4.5.0/"
     FORMAT=".zip"
 
     if [ `uname` = "Linux" ]; then
-        echo "Linux"
+        echo "getting z3 for " `uname`
         EXECUTABLE="z3-4.5.0-x64-ubuntu-14.04"
 
 
     elif [ `uname` = "Darwin" ]; then
-        echo "Darwin"
+        echo "getting z3 for " `uname`
         EXECUTABLE="z3-4.5.0-x64-osx-10.11.6"
     fi
 
