7e260a9b977e32028c8970438f6fb80b29964b9c
Manasvi Saxena <msaxena2@illinois.edu>
2017-05-23 20:15:25 -0500

    adapt shell script for codeship

--- installZ3.sh
+++ installZ3.sh
@@ -1,21 +1,18 @@
 #!/bin/sh
 
-rm -r z3*
 if [ ! -d ./z3 ]; then
 
     URL="https://github.com/Z3Prover/z3/releases/download/z3-4.5.0/"
     FORMAT=".zip"
 
     if [ `uname` = "Linux" ]; then
-        echo "getting z3 for " `uname`
         EXECUTABLE="z3-4.5.0-x64-ubuntu-14.04"
 
 
     elif [ `uname` = "Darwin" ]; then
-        echo "getting z3 for " `uname`
         EXECUTABLE="z3-4.5.0-x64-osx-10.11.6"
     fi
 
-    curl -OL $URL$EXECUTABLE$FORMAT && tar -xvf $EXECUTABLE$FORMAT && mv $EXECUTABLE"/" z3 && rm $EXECUTABLE$FORMAT
+    curl -OL $URL$EXECUTABLE$FORMAT && unzip $EXECUTABLE$FORMAT && mv $EXECUTABLE"/" z3 && rm $EXECUTABLE$FORMAT
 
 fi
