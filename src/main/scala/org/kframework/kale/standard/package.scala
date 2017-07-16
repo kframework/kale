8812ec41a23a58bea6a0d28555b4bf042887b664
Cosmin Radoi <cosmin.radoi@gmail.com>
2017-07-16 14:14:39 -0500

    fixes

--- src/main/scala/org/kframework/kale/standard/package.scala
+++ src/main/scala/org/kframework/kale/standard/package.scala
@@ -6,15 +6,21 @@ package object standard {
   def lift(funcName: String, func: Term => Term)(implicit oenv: Environment): Label1 =
     new Named(funcName) with Label1 {
       override def apply(_1: Term): Term = func(_1)
+
+      val isPredicate = true
     }
 
   def lift(funcName: String, func: (Term, Term) => Term)(implicit oenv: Environment): Label2 =
     new Named(funcName) with Label2 {
       override def apply(_1: Term, _2: Term): Term = func(_1, _2)
+
+      val isPredicate = true
     }
 
   def lift(funcName: String, func: Term => Option[Term])(implicit oenv: Environment) =
     new Named(funcName) with FunctionLabel1 {
       override def f(_1: Term): Option[Term] = func(_1)
+
+      val isPredicate = true
     }
 }
