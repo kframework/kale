1f40095ebdf33d2f74aae48288ebe7f8f96aa774
cos <cosmin.radoi@gmail.com>
2017-10-09 14:53:35 -0700

    expriments -- leave here for future

--- src/main/scala/org/kframework/kale/transformer/Binary.scala
+++ src/main/scala/org/kframework/kale/transformer/Binary.scala
@@ -2,6 +2,7 @@ package org.kframework.kale.transformer
 
 import org.kframework.kale._
 import org.kframework.kale.standard.StandardEnvironment
+import org.kframework.kale.util.measureTime
 
 object Binary {
 
@@ -43,23 +44,45 @@ object Binary {
       arr
     }
 
+    protected lazy val m1: Map[(Int, Int), (Term, Term) => Term] = {
+      val pf = processingFunctions.lift
+
+      (for (left <- env.labels;
+            right <- env.labels) yield {
+        (left.id, right.id) -> pf((left, right)).map(x => x(this)).orNull
+      }) toMap
+    }
+
+    // m2 seems to be most performant
+    protected lazy val m2: Map[Int, Map[Int, (Term, Term) => Term]] = {
+      val pf = processingFunctions.lift
+
+      (for (left <- env.labels) yield {
+        left.id ->
+          (for (right <- env.labels) yield {
+            right.id -> pf((left, right)).map(x => x(this)).orNull
+          }).toMap
+      }).toMap
+    }
+
     val statsInvocations = collection.mutable.Map[(Term, Term) => Term, Int]().withDefaultValue(0)
 
     val memo = collection.mutable.Map[(Term, Term), Term]()
 
     def functionFor(left: Label, right: Label): (Term, Term) => Term = {
       try {
-        arr(left.id)(right.id)
+        //        arr(left.id)(right.id)
+        //        m1.get((left.id, right.id)).orNull
+        m2.get(left.id).flatMap(_.get(right.id)).orNull
       } catch {
         case _: IndexOutOfBoundsException => throw new AssertionError("No processing function registered for: " + left + " and " + right)
       }
     }
 
-    def apply(left: Term, right: Term): Term = {
-
+    def apply(left: Term, right: Term): Term = measureTime("unify") {
       //      assert(labels.contains(left.label) && labels.contains(right.label))
-      assert(left.label.id <= env.labels.size, "Left label " + left.label + " with id " + left.label.id + " is not registered. Label list:" + env.labels.map(l => (l.id, l)).toList.sortBy(_._1).mkString("\n"))
-      assert(right.label.id <= env.labels.size, "Right label " + right.label + " with id " + right.label.id + " is not registered. Label list:" + env.labels.map(l => (l.id, l)).toList.sortBy(_._1).mkString("\n"))
+      //      assert(left.label.id <= env.labels.size, "Left label " + left.label + " with id " + left.label.id + " is not registered. Label list:" + env.labels.map(l => (l.id, l)).toList.sortBy(_._1).mkString("\n"))
+      //      assert(right.label.id <= env.labels.size, "Right label " + right.label + " with id " + right.label.id + " is not registered. Label list:" + env.labels.map(l => (l.id, l)).toList.sortBy(_._1).mkString("\n"))
 
       if (left == right)
         return right
@@ -82,7 +105,9 @@ object Binary {
       }
     }).toMap
 
-    override def toString: String = processingFunctionsByLabelPair.mkString("\n")
+    override def toString: String
+
+    = processingFunctionsByLabelPair.mkString("\n")
   }
 
 }
