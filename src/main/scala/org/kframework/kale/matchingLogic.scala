226f2848047dfbe166c31d9b0b0844d19cd0b743
cos <cosmin.radoi@gmail.com>
2017-10-12 15:20:45 -0700

    fix warning

--- src/main/scala/org/kframework/kale/matchingLogic.scala
+++ src/main/scala/org/kframework/kale/matchingLogic.scala
@@ -1,6 +1,6 @@
 package org.kframework.kale
 
-import org.kframework.kale.standard.{MightBeSolved, Name, Sort}
+import org.kframework.kale.standard.{MightBeSolved, Sort}
 import org.kframework.kore.implementation.DefaultBuilders
 import org.kframework.{kale, kore}
 
@@ -70,9 +70,9 @@ trait Sort extends kore.Sort {
 }
 
 trait VariableLabel extends LeafLabel[(Name, Sort)] {
-  def apply(name: String): Variable = apply((Name(name), Sort.K))
+  def apply(name: String): Variable = apply((standard.Name(name), Sort.K))
 
-  def apply(name: String, sort: kale.Sort): Variable = apply((Name(name), sort))
+  def apply(name: String, sort: kale.Sort): Variable = apply((standard.Name(name), sort))
 
   def apply(v: (Name, Sort)): Variable
 
